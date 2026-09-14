d864370a1ecfd0456e623687a005d899502601a6
TELLOWSS <pshkmj@gmail.com>
2026-09-15 04:17:45 +0900

    TASK-008G: add instruction content bundle

---
+++ src/content/episode01-instruction.ts
@@ -0,0 +1,5 @@
+import events from '../../content/episode01/instruction-chain-events.json';
+import ko from '../../content/episode01/instruction-ko.json';
+
+export const episode01InstructionEvents = events;
+export const episode01InstructionMessages = ko.messages;
