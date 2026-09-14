549ad59df89366390be6d34aee12a3c253e09c60
TELLOWSS <pshkmj@gmail.com>
2026-09-15 04:28:47 +0900

    TASK-008H: expose record pressure content bundle

---
+++ src/content/episode01-record.ts
@@ -0,0 +1,5 @@
+import recordEvents from '../../content/episode01/record-pressure-events.json';
+import recordKo from '../../content/episode01/record-pressure-ko.json';
+
+export const episode01RecordEvents = recordEvents;
+export const episode01RecordMessages = recordKo.messages;
