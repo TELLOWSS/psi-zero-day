4f0f1256512afc5c2b0726e83b89c7f16ad3732f
TELLOWSS <pshkmj@gmail.com>
2026-09-15 03:32:51 +0900

    TASK-008D: add TBM content bundle

---
+++ src/content/episode01-tbm.ts
@@ -0,0 +1,5 @@
+import tbmGapEvents from '../../content/episode01/tbm-gap-events.json';
+import tbmGapKo from '../../content/episode01/tbm-gap-ko.json';
+
+export const episode01TbmGapEvents = tbmGapEvents;
+export const episode01TbmGapMessages = tbmGapKo.messages;
