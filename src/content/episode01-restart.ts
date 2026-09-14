ce38026d9335ef8932981d3e658f06671be04d2b
TELLOWSS <pshkmj@gmail.com>
2026-09-15 03:59:03 +0900

    TASK-008E: expose restart content bundle

---
+++ src/content/episode01-restart.ts
@@ -0,0 +1,5 @@
+import restartEvents from '../../content/episode01/restart-events.json';
+import restartKo from '../../content/episode01/restart-ko.json';
+
+export const episode01RestartEvents = restartEvents;
+export const episode01RestartMessages = restartKo.messages;
