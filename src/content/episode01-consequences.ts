fb9993187f920a78b7ca42e31a69d759221f103f
TELLOWSS <pshkmj@gmail.com>
2026-09-15 04:41:45 +0900

    TASK-009A: make consequence assembly routing-neutral

--- src/content/episode01-consequences.ts
+++ src/content/episode01-consequences.ts
@@ -9,13 +9,8 @@ export function assembleEpisode01Consequences(
   instructionEvents: readonly any[],
   recordEvents: readonly any[],
 ) {
-  const eveningGate = { kind: 'event_completed', event_id: 'e01_08p_record_return', minimum_count: 1 };
-  return baseEvents.flatMap(event => {
-    const next = event.event_id === 'e01_09_evening'
-      ? { ...event, conditions: [...event.conditions, eveningGate] }
-      : event;
-    return event.event_id === 'e01_08_reactions'
-      ? [next, ...consequenceEvents, ...inspectionEvents, ...responsibilityEvents, ...tbmGapEvents, ...restartEvents, ...stopworkEvents, ...instructionEvents, ...recordEvents]
-      : [next];
-  });
+  return baseEvents.flatMap(event => event.event_id === 'e01_08_reactions'
+    ? [event, ...consequenceEvents, ...inspectionEvents, ...responsibilityEvents, ...tbmGapEvents,
+      ...restartEvents, ...stopworkEvents, ...instructionEvents, ...recordEvents]
+    : [event]);
 }
