1465f21d1df96010d77c4e2b92b7f6004f6f9570
TELLOWSS <pshkmj@gmail.com>
2026-09-15 04:09:14 +0900

    TASK-008F: add stop-work content bundle

---
+++ src/content/episode01-stopwork.ts
@@ -0,0 +1,5 @@
+import stopworkEvents from '../../content/episode01/stopwork-aftershock-events.json';
+import stopworkKo from '../../content/episode01/stopwork-ko.json';
+
+export const episode01StopworkEvents = stopworkEvents;
+export const episode01StopworkMessages = stopworkKo.messages;
