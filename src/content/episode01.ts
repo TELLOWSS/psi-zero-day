919de9e072a8c75559e91df9e4131992e1d0c1e6
TELLOWSS <pshkmj@gmail.com>
2026-09-15 03:24:13 +0900

    TASK-008C: assemble responsibility chain into Episode 01

--- src/content/episode01.ts
+++ src/content/episode01.ts
@@ -1,13 +1,18 @@
 import manifest from '../../content/episode01/manifest.json';
 import characters from '../../content/episode01/characters.json';
 import inspectionCharacters from '../../content/episode01/inspection-characters.json';
+import responsibilityCharacters from '../../content/episode01/responsibility-characters.json';
 import relations from '../../content/episode01/relations.json';
 import inspectionRelations from '../../content/episode01/inspection-relations.json';
+import responsibilityRelations from '../../content/episode01/responsibility-relations.json';
 import events from '../../content/episode01/events.json';
 import consequenceEvents from '../../content/episode01/consequence-events.json';
 import inspectionEvents from '../../content/episode01/inspection-events.json';
+import responsibilityClashEvent from '../../content/episode01/responsibility-clash-event.json';
+import reportReturnEvent from '../../content/episode01/report-return-event.json';
 import ko from '../../content/episode01/ko.json';
 import inspectionKo from '../../content/episode01/inspection-ko.json';
+import responsibilityKo from '../../content/episode01/responsibility-ko.json';
 import { ContentRegistry } from './registry';
 import { assembleEpisode01Consequences } from './episode01-consequences';
 
@@ -15,10 +20,10 @@ import { assembleEpisode01Consequences } from './episode01-consequences';
 export function createEpisode01Registry(): ContentRegistry {
   return new ContentRegistry({
     ...manifest.bundle,
-    localizations: [{ ...ko, messages: { ...ko.messages, ...inspectionKo.messages } }],
-    characters: [...characters, ...inspectionCharacters],
-    relations: [...relations, ...inspectionRelations],
-    events: assembleEpisode01Consequences(events, consequenceEvents, inspectionEvents),
+    localizations: [{ ...ko, messages: { ...ko.messages, ...inspectionKo.messages, ...responsibilityKo.messages } }],
+    characters: [...characters, ...inspectionCharacters, ...responsibilityCharacters],
+    relations: [...relations, ...inspectionRelations, ...responsibilityRelations],
+    events: assembleEpisode01Consequences(events, consequenceEvents, inspectionEvents, [responsibilityClashEvent, reportReturnEvent]),
   });
 }
 
